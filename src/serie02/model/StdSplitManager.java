e5e29fb480d6df6abcdc08487ceace672d18f785
ba-talibe <omarba2015@gmail.com>
2023-10-03 16:08:07 +0200

    corrected constructor

--- src/serie02/model/StdSplitManager.java
+++ src/serie02/model/StdSplitManager.java
@@ -18,7 +18,6 @@ import util.Contract;
 public class StdSplitManager implements SplitManager {
 
 	private File file;
-	private static final int EOF = -1; 
 	private List<Long> sizes = new ArrayList<Long>();
 	
 	public StdSplitManager(){
@@ -29,30 +28,32 @@ public class StdSplitManager implements SplitManager {
 	public StdSplitManager(File file){
 		Contract.checkCondition(file != null);
 		this.file = file;
-				
+		this.sizes.add(file.length());
 	}
 
 	@Override
 	public boolean canSplit() {
-		return FileStateTester.isSplittable(this.file);
+		if (FileStateTester.isSplittable(this.file) == true){
+			return true;
+		}
+		return false;
 	}
 
 	@Override
 	public String getDescription() {
-		
+
 		return FileStateTester.describe(this.file);
 	}
 
 	@Override
 	public File getFile() {
-		
 		return this.file;
 	}
 
 	@Override
 	public int getMaxFragmentNb() {
 		Contract.checkCondition(canSplit());
-		return (int) Math.min(MAX_FRAGMENT_NB, Math.max(1, Math.floor(file.length()/MIN_FRAGMENT_SIZE)));
+		return (int) Math.min(MAX_FRAGMENT_NB, Math.max(1, Math.floor(file.length()/getMinFragmentSize())));
 	}
 
 	@Override
@@ -64,40 +65,50 @@ public class StdSplitManager implements SplitManager {
 	@Override
 	public String[] getSplitsNames() {
 		Contract.checkCondition(canSplit());
-		return null;
+		int splitsNb = this.sizes.size();
+		String[] splitsName = new String[splitsNb];
+		for (int i=0;i<splitsNb; i++){
+			splitsName[i] = getFile().getAbsolutePath() + "." + (i+1);
+		}
+		return splitsName;
 	}
 
 	@Override
 	public long[] getSplitsSizes() {
-		
-		return null;
+		int N = this.sizes.size();
+		long[] splitsSizes = new long[N];
+		for (int i=0 ;i<N; i++){
+			splitsSizes[i] = this.sizes.get(i);
+		}
+		return splitsSizes;
 	}
 
 	@Override
 	public void changeFor(File f) {
 		Contract.checkCondition(f != null);
 		this.file = f;
+		this.sizes.clear();
 	}
 
 	@Override
 	public void close() {
-		
-
+		this.file = null;
 	}
 
 	@Override
 	public void setSplitsNumber(int splitsNb) {
 		Contract.checkCondition(canSplit());
 		Contract.checkCondition(1 <= splitsNb && splitsNb<= getMaxFragmentNb());
+		this.sizes.clear();
 		long q = (long) Math.floor(this.file.length()/splitsNb);
 		long r = (long) this.file.length() % splitsNb;
 		for (int i=0; i<r; i++ ) {
 			this.sizes.add(q+1) ;
 		}
 		for (int i=(int)r; i <splitsNb; i++) {
-			this.sizes.add(q) ;
+			this.sizes.add(q);
 		}
-		
+		//System.out.println("setSplitsSizes int " + splitsNb + " size " + this.sizes);
 	}
 
 	@Override
@@ -105,15 +116,14 @@ public class StdSplitManager implements SplitManager {
 		Contract.checkCondition(canSplit());
 		Contract.checkCondition(getMinFragmentSize() <= size);
 		Contract.checkCondition(size <= this.file.length());
-		
+		this.sizes.clear();
 		double k = Math.floor(this.file.length() / size);
 		double r = this.file.length() % size;
 		double splitsNb = (r == 0) ? k : k + 1;
 		for (double i=0; i<splitsNb-1; i++){
 			this.sizes.add(size);
 		}
-		this.sizes.add((int) splitsNb, (r == 0) ? size : (long) r);
-
+		this.sizes.add((int) splitsNb-1, (r == 0) ? size : (long) r);
 	}
 
 	private long sumSizes(long[] sizes){
@@ -129,8 +139,9 @@ public class StdSplitManager implements SplitManager {
 	public void setSplitsSizes(long[] sizes) {
 		Contract.checkCondition(canSplit());
 		Contract.checkCondition(sizes != null);
-		Contract.checkCondition(sizes.length >= 1);
+		Contract.checkCondition(sizes.length > 1);
 		Contract.checkCondition(sizes.length < getMaxFragmentNb());
+		this.sizes.clear();
 		for (int i=0;i<sizes.length;i++){
 			Contract.checkCondition(sizes[i] >= getMinFragmentSize());
 			Contract.checkCondition(sizes[i] < this.file.length());
@@ -148,7 +159,7 @@ public class StdSplitManager implements SplitManager {
 		}else{
 			sum = 0;
 			for (int i=0;i<sizes.length;i++){
-				this.sizes.add(sizes[i]);
+				this.sizes.add(i, sizes[i]);
 				sum += sizes[i];
 				if (sum >= n){
 					if (sum == n)
@@ -158,7 +169,7 @@ public class StdSplitManager implements SplitManager {
 				}
 			}
 		}
-		
+		//System.out.println("setSplitsSizes long[] " + sizes + " size " + this.sizes);
 	}
 
 	private InputStream createInputStream() throws FileNotFoundException{
@@ -169,7 +180,7 @@ public class StdSplitManager implements SplitManager {
 	}
 	private OutputStream createOutputstream(long n) throws FileNotFoundException{
 		return new BufferedOutputStream(
-			new FileOutputStream(getFile().getAbsolutePath() + "." + n)
+			new FileOutputStream(getFile().getAbsolutePath() + "." + (n+1))
 		);
 	}
 
@@ -178,18 +189,27 @@ public class StdSplitManager implements SplitManager {
 		Contract.checkCondition(canSplit());
 		InputStream input = createInputStream();
 		OutputStream output;
-		
-		for ( int i=0;i<this.sizes.size(); i++){
+
+		try {
+			for ( int i=0;i<this.sizes.size(); i++){
 			long size = this.sizes.get(i);
 			byte[] readBytes = new byte[(int) size];
 			if (input.read(readBytes) != -1){
 				output = createOutputstream(i);
 				output.write(readBytes);
-			}else{
-				throw new IOException();
+				output.flush();
+				output.close();
+				}
 			}
 
+		} catch(FileNotFoundException e){
+			System.out.println(e.getMessage());
+		}catch (IOException e) {
+			System.out.println(e.getMessage());
 		}
+		
+		
 	}
-
 }
+
+
